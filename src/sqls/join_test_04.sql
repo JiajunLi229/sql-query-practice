7e083c50f208fec3e99332b7a74352b26104b5b2
Jiajun <398160658@qq.com>
2020-06-25 22:57:38 +0800

    [JiajunLi] feat: should select customer and order count

--- src/sqls/join_test_04.sql
+++ src/sqls/join_test_04.sql
@@ -8,3 +8,11 @@
  *
  * 查询结果请按照 `customerNumber` 排序。
  */
+SELECT
+`customers`.`customerNumber`,
+COUNT(`orders`.`orderNumber`) AS `totalOrders`
+FROM `customers`
+LEFT JOIN `orders`
+ON `customers`.`customerNumber` = `orders`.`customerNumber`
+WHERE `customers`.`customerNumber` < 150
+group by `customers`.`customerNumber`;
