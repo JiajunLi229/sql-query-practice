ae1278269118ed25979bcabd3ab377198964a6bf
Jiajun <398160658@qq.com>
2020-06-25 22:36:50 +0800

    [JiajunLi] feat: should select order details of which the price is lower than suggested price

--- src/sqls/join_test_03.sql
+++ src/sqls/join_test_03.sql
@@ -9,3 +9,13 @@
  * 
  * 结果应当按照 `orderNumber` 排序。
  */
+SELECT 
+`orderdetails`.`orderNumber`, 
+`productName`, 
+`MSRP`, 
+`orderdetails`.`priceEach`
+FROM `products`
+INNER JOIN `orderdetails`
+ON `products`.`productCode` = `orderdetails`.`productCode`
+WHERE `products`.`productCode` = 'S10_1678' AND `priceEach` < `MSRP`
+GROUP BY `orderNumber`;
